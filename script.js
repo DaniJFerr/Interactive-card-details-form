0d066e4fbe8b127a2c69b947ea4d16323a35eff7
DaniJFerreira <daniel.jorge.ferreira1984@gmail.com>
2023-07-04 17:09:33 +0100

    fix the the card image size for mobile view

--- script.js
+++ script.js
@@ -1,96 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const confirmButton = document.querySelector('button');
-//     const cardNumberInput = document.querySelector('.card-number input');
-//     const cardNameInput = document.querySelector('.card-name input');
-//     const dateCardCvcInputs = document.querySelectorAll('.date-card-cvc input');
-//     const confirmEvent = document.querySelector('.confirmation');
-//     const formEvent = document.querySelector('.formEvent');
-//     const cardNumberElement = document.querySelector('#Card-front h1');
-//     const cardNameElement = document.querySelector('#Card-front h2');
-//     const cardDateElement = document.querySelector('#Card-front p');
-//     const cardCvcElement = document.querySelector('#Card-back .cardCvc');
-  
-//     confirmButton.addEventListener('click', function(event) {
-//       event.preventDefault();
-
-//       // Check if all fields are filled in and meet the requirements
-//       const cardNumberValue = cardNumberInput.value.trim().replace(/\s+/g, '');
-//       const formattedCardNumber = cardNumberValue.replace(/(.{4})/g, '$1 ');
-//       const isValidCardName = cardNameInput.value !== '';
-//       const isValidCardNumber = cardNumberInput.value.length === 16 && !isNaN(cardNumberInput.value.trim());
-//       const isValidMonth = dateCardCvcInputs[0].value !== '';
-//       const isValidYear = dateCardCvcInputs[1].value !== '';
-//       const isValidCVC = dateCardCvcInputs[2].value !== '' && dateCardCvcInputs[2].value.length === 3;
-
-//         // Handle input errors or restore original style
-//         if (cardNameInput.value === '' || cardNumberInput.value === '' || dateCardCvcInputs[0].value === '' || dateCardCvcInputs[1].value === '' || dateCardCvcInputs[2].value === '' || !isValidCardName || !isValidCardNumber || !isValidMonth || !isValidYear || !isValidCVC) {
-//           handleInputError(cardNameInput, 'Please fill your name on card.');
-//           handleInputError(cardNumberInput, 'Please fill the card number.');
-//           handleInputError(dateCardCvcInputs[0], 'Please fill in a month.');
-//           handleInputError(dateCardCvcInputs[1], 'Please fill in a year.');
-//           handleInputError(dateCardCvcInputs[2], 'Please fill in the CVC or insert max 3 digits.');
-
-//           return;
-//         }else if (cardNumberInput.value.length !== 16){
-//           handleInputError(cardNumberInput, 'Please fill in the card number with 16 digits.');
-
-//           return;
-//         }else{       
-//              // Restore original style
-//           restoreOriginalStyle(cardNameInput);
-//           restoreOriginalStyle(cardNumberInput);
-//           restoreOriginalStyle(dateCardCvcInputs[0]);
-//           restoreOriginalStyle(dateCardCvcInputs[1]);
-//           restoreOriginalStyle(dateCardCvcInputs[2]);
-
-//       // All fields are filled in, remove error messages
-//       const errorMessages = document.querySelectorAll('.error');
-//       errorMessages.forEach(msg => msg.remove());
-          
-//       // Update card elements with input values
-//       const cardDate =`${dateCardCvcInputs[0].value}/${dateCardCvcInputs[1].value}`;
-  
-//       cardNumberElement.innerText = formattedCardNumber;
-//       cardNameElement.innerText = cardNameInput.value;
-//       cardDateElement.innerText = cardDate;
-//       cardCvcElement.innerText = dateCardCvcInputs[2].value;
-//       confirmEvent.removeAttribute('hidden');
-//       formEvent.setAttribute('hidden', '');
-//       document.getElementById('cardholder').style.height = "100px";
-//       document.querySelector('.button').classList.add('onclic');
-//       setTimeout(function() {
-//         document.querySelector('.button').classList.remove('onclic');
-//         document.querySelector('.button').classList.add('validate');
-//         setTimeout( function() {  
-//           document.querySelector('.button').classList.remove('validate');
-//         }, 1250);
-//       }, 1250);
-
-//       return;
-//       }
-  
-//     });
-  
-//     function handleInputError(input, message) {
-//       if (!input.parentElement.querySelector('.error')) {
-//         const error = document.createElement('p');
-//         error.classList.add('error')      
-//         error.textContent = message;
-//         error.style.color = 'red';
-//         input.parentElement.appendChild(error);
-//         input.style.border='2px solid red';
-//       }
-//     }
-  
-//     function restoreOriginalStyle(input) {
-//       const errorMessage = input.parentElement.querySelector('.error');
-//       if (errorMessage) {
-//         errorMessage.remove();
-//       }
-//       input.style.border= '';
-//      }
-//     });
-
 
 document.addEventListener('DOMContentLoaded', function() {
   const confirmButton = document.querySelector('button');
@@ -118,13 +25,13 @@ document.addEventListener('DOMContentLoaded', function() {
       handleInputError(dateCardCvcInputs[1], 'Please fill in a year.');
       handleInputError(dateCardCvcInputs[2], 'Please fill in the CVC or insert max 3 digits.');
       return;
+    }else{  
+      restoreOriginalStyle(cardNameInput);
+      restoreOriginalStyle(cardNumberInput);
+      restoreOriginalStyle(dateCardCvcInputs[0]);
+      restoreOriginalStyle(dateCardCvcInputs[1]);
+      restoreOriginalStyle(dateCardCvcInputs[2]);
     }
-
-    restoreOriginalStyle(cardNameInput);
-    restoreOriginalStyle(cardNumberInput);
-    restoreOriginalStyle(dateCardCvcInputs[0]);
-    restoreOriginalStyle(dateCardCvcInputs[1]);
-    restoreOriginalStyle(dateCardCvcInputs[2]);
 
     const errorMessages = document.querySelectorAll('.error');
     errorMessages.forEach(msg => msg.remove());
